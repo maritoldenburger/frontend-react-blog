635ea3ab184b12759a2c1243204fea31b201fe75
moldenburger <maritoldenburger@hotmail.com>
2025-03-18 15:40:18 +0100

    Finished Opdracht 2 (getPosts)

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,12 +1,37 @@
 import "./Overview.css";
-import posts from "../../constants/data.json";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Overview() {
+
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const getPosts = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("http://localhost:3000/posts");
+            setPosts(response.data);
+        } catch (err) {
+            setError(err);
+            console.error(err.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, []);
+
     return (
         <>
             <h1>Bekijk alle {posts.length} blogs op het platform</h1>
-                {posts.map(post => (
+            {loading && <span className="loader"/>}
+            {error && <p>Error: {error}</p>}
+            {posts.map(post => (
                 <div key={post.id} className="post-tile">
                     <h2><Link className="title-link" to={`/posts/${post.id}`}>{post.title} </Link>
                         ({post.author})</h2>
